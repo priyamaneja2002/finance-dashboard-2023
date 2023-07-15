d06005d9295e1b6a7a32b534c7bf938b78f85acd
Priyam Aneja <priyamaneja2002@gmail.com>
2023-07-16 04:49:21 +0530

    Connected database, data modelling and KPIs Route

--- client/src/scenes/dashboard/row1.tsx
+++ client/src/scenes/dashboard/row1.tsx
@@ -5,6 +5,7 @@ type Props = {}
 
 const Row1 = (props: Props) => {
   const { data } = useGetKpisQuery();
+  console.log('data:', data);
 
   return (
     <>
--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import bodyParser from "body-parser";
-import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
-
+import mongoose from "mongoose";
+import kpiRoutes from "./routes/kpi.js";
+import KPI from "./models/KPI.js";
+import { kpis } from "./data/data.js";
 // Configurations
 
 dotenv.config();
@@ -18,4 +20,22 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded( { extended: false } ));
 app.use(cors());
 
+// Routes
+app.use('/kpi', kpiRoutes);
+
 /* Mongoose setup */
+const PORT = process.env.PORT || 9000;
+app.listen(PORT, () => console.log(`server connected at Port: ${PORT}`));
+mongoose
+    .connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(async () => {
+        console.log('Database connected');
+        // await mongoose.connection.db.dropDatabase();
+        // KPI.insertMany(kpis);
+        // Product.insertMany(products);
+        // Transaction.insertMany(transactions);
+    })
+    .catch((error) => console.log(`${error}, did not connect`));
